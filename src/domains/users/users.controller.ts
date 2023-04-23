1cb1bc61764713219735b13bbad2489e62b4e0f1
chan9yu <wry5533@gmail.com>
2023-04-23 11:09:55 +0900

    feat: 스웨거 api 문서 작성 시작

--- src/domains/users/users.controller.ts
+++ src/domains/users/users.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+import { ReadOnlyUserDto } from './dto/users.dto';
 import { UserRequestDto } from './dto/users.request.dto';
 import { UsersService } from './users.service';
 
@@ -7,26 +9,33 @@ import { UsersService } from './users.service';
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
+	@ApiOperation({ summary: '내 정보 조회', tags: ['users'] })
 	@Get()
 	getCurrentUser() {
 		return 'getCurrentUser';
 	}
 
+	@ApiOperation({ summary: '회원가입', tags: ['users'] })
+	@ApiResponse({ status: 200, description: 'Sussess', type: ReadOnlyUserDto })
+	@ApiResponse({ status: 500, description: 'Server Error' })
 	@Post('signup')
 	async signUp(@Body() userRequestDto: UserRequestDto) {
 		return await this.usersService.createUser(userRequestDto);
 	}
 
+	@ApiOperation({ summary: '로그인', tags: ['users'] })
 	@Post('login')
 	async login() {
 		return 'login';
 	}
 
+	@ApiOperation({ summary: '로그아웃', tags: ['users'] })
 	@Post('logout')
 	async logout() {
 		return 'logout';
 	}
 
+	@ApiOperation({ summary: '프로필 이미지 업로드', tags: ['users'] })
 	@Post('upload/user')
 	async uploadUserImg() {
 		return 'uploadUserImg';
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	const globalPrefix = 'api' as const;
-	const port = process.env.PORT || 3333;
+	const GLOBAL_PREFIX = 'api' as const;
+	const PORT = process.env.PORT || 3333;
 
-	app.setGlobalPrefix(globalPrefix);
+	app.setGlobalPrefix(GLOBAL_PREFIX);
 	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors({ origin: true, credentials: true });
 
-	await app.listen(port);
-	Logger.log(`🚀 어플리케이션을 시작합니다... URL: http://localhost:${port}/api`);
+	const config = new DocumentBuilder()
+		.setTitle('dev-twitter')
+		.setDescription('dev twitter API description')
+		.setVersion('1.0.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, document);
+
+	await app.listen(PORT);
+	Logger.log(`🚀 어플리케이션을 시작합니다 http://localhost:${PORT}/api`);
+	Logger.log(`🗂️  Swagger API 문서 http://localhost:${PORT}/docs`);
 }
 
 bootstrap();
